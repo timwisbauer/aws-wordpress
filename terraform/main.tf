ff1398550540beb0a9517d94e0402dc05bc98972
Tim Wisbauer <tim.wisbauer@gmail.com>
2020-09-22 16:41:53 -0500

    Added basic EC2 configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
+##################################################################
+# Provider and backend configuration
+##################################################################
+
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
@@ -10,8 +14,13 @@ terraform {
   }
 }
 
+##################################################################
+# Core networking configuration
+##################################################################
+
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> v2.0"
 
   name = "tf-ansible-wordpress"
   cidr = "10.0.0.0/16"
@@ -23,8 +32,60 @@ module "vpc" {
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
     Application = "wordpress"
   }
 }
+
+##################################################################
+# EC2 instances
+##################################################################
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+module "wordpress_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  for_each = toset(module.vpc.private_subnets)
+
+  name           = "wordpress"
+  instance_count = 1
+
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = each.value
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Applicatipn = "wordpress"
+  }
+}
+
+##################################################################
+# ALB configuration
+##################################################################
